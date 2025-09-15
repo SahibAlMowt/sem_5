0d14191423e685353873bc04d5ceb21a5fbbf1d8
SahibAlMowt <sahibshukurlu2006@gmail.com>
2025-09-15 20:27:22 +0400

    ash nazg

--- task2.cpp
+++ task2.cpp
@@ -44,5 +44,55 @@ int main()
         min_r = X;
     }
 
+    int light = 0;
+
+    while(light < N && max_num > 0)
+    {
+        int pos;
+        pos = light + max_r + 1;
+
+        if(pos > N)
+        {
+            pos = N;
+        }
+
+        lamps.push_back({pos, max_r});
+
+        light = pos + max_r;
+
+        max_num--;
+
+    }
+
+
+    while(light < N && min_num > 0)
+    {
+        int pos;
+        pos = light + min_r + 1;
+
+        if(pos > N)
+        {
+            pos = N;
+        }
+
+        lamps.push_back({pos, min_r});
+
+        light = pos + min_r;
+
+        min_num--;
+    }
+
+
+    if(light < N)
+    {
+        std::cout << "-1\n";
+        return 0;
+    }
+
+    for(size_t i = 0; i != lamps.size(); i++)
+    {
+        std::cout << lamps[i].position << " " << lamps[i].radius << "\n";
+    }
+
     return 0;
 }
